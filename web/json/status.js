9f67fb4ab2178923912023602a056985098a4f91
lukaijie <lukaijie@shopex.cn>
2017-07-07 23:37:50 +0800

    m

--- web/json/status.js
+++ web/json/status.js
@@ -6,9 +6,9 @@ var status_list = [
     // { status_id: 5, color: '#797979', name: '调试' }1782c7
     { status_id: 1, color: '#e9513d', name: '停机' },
     { status_id: 2, color: '#3fb756', name: '运行' },
-    { status_id: 3, color: '#d68d26', name: '空闲' },
-    { status_id: 4, color: '#b0aaaa', name: '离线' },
-    { status_id: 5, color: '#1782c7', name: '调试' }
+    { status_id: 3, color: '#d68d26', name: '空闲' }
+    // { status_id: 4, color: '#b0aaaa', name: '离线' },
+    // { status_id: 5, color: '#1782c7', name: '调试' }
 ]
 if ( typeof module != 'undefined' ) {
     module.exports = status_list;
