61f1d6403b71b6b3a775129343612d2a1323d133
lukaijie <lukaijie@shopex.cn>
2017-08-10 13:51:00 +0800

    m

---
+++ app_visual.js
@@ -0,0 +1,94 @@
+﻿var express = require( 'express' );
+var path = require( 'path' );
+var app = express();
+var server = require( 'http' ).Server( app );
+// var io = require('socket.io')(server);
+var request = require( 'request' );
+var _ = require( 'underscore' );
+var rf = require( "fs" );
+var io = require( 'socket.io' )( server );
+var rooms = {
+    1: 'a',
+    2: 'b'
+}
+var server_url = "http://192.168.20.235:8688/Module/SystemData/YuJiangBoard.asmx"
+
+app.use( express.static( path.join( __dirname, 'web' ) ) );
+server.listen( 5000, function () {
+    console.log( 'server staring..., port: 5000' );
+} );
+
+app.get( '/room', function ( req, res ) {
+    if ( req.query.v == 1 ) {
+        res.sendFile( __dirname + '/web/demo1.html' );
+    }
+} );
+
+io.on( 'connection', function ( socket ) {
+    var roomid = getParam( socket.request.headers.referer.split( '?' )[1], "v" )
+    if ( typeof rooms[roomid] != 'undefined' ) {
+        socket.join( roomid );    // 加入房间
+    }
+    //socket.emit('news', { port: port });
+    // socket.on('my other event', function(data) {
+    //     console.log(data);
+    // });
+    socket.on( 'disconnect', function () {
+        io.emit( 'user disconnected' );
+    } );//
+} );
+
+function getParam( url, name ) {
+    var reg = new RegExp( "(^|&)" + name + "=([^&]*)(&|$)", "i" );
+    var r = url.match( reg );
+    if ( r != null ) return unescape( r[2] );
+    return null;
+}
+
+var room = ["a", "b"]
+setInterval( function () {
+    getMacStatus_s( 30056, function ( res ) {
+        console.log( res );
+        io.to( 1 ).emit( 'getMacStatus', res )
+    } )
+}, 5000 )
+
+setInterval( function () {
+    getAlarmInfo_s( 30056, function ( res ) {
+        io.to( 1 ).emit( 'getAlarmInfo', res )
+    } )
+    getStatusRatio_s( 30056, function ( res ) {
+        io.to( 1 ).emit( 'getStatusRatio', res )
+    } )
+}, 10000 )
+
+function getMacStatus_s( room, callback ) {
+    request.post( server_url + '/getMacStatus',
+        { form: { pid: room } }, function ( error, response, data ) {
+            var d = JSON.parse( data );
+            console.log('/getMacStatus')
+            console.log(d.result)
+            callback( d.result )
+        } )
+}
+
+function getAlarmInfo_s( room, callback ) {
+    request.post( server_url + '/getAlarmInfo',
+        { form: { pid: room } }, function ( error, response, data ) {
+            var d = JSON.parse( data );
+            console.log('/getAlarmInfo')
+            console.log(d.result)
+            callback( d.result )
+        } )
+}
+
+function getStatusRatio_s( room, callback ) {
+    request.post( server_url + '/getStatusRatio',
+        { form: { pid: room } }, function ( error, response, data ) {
+            var d = JSON.parse( data );
+            console.log('/getStatusRatio')
+            console.log(d.result)
+            callback( d.result )
+        } )
+}
+
--- app_visual1.js
+++ app_visual1.js
@@ -10,8 +10,8 @@ var io = require( 'socket.io' )( server );
 var rooms = {
     1: 'a',
     2: 'b'
-}
-var server_url = "http://192.168.60.2:8088/api/services/app/wimiVisual"
+};
+var server_url = "http://192.168.20.235:8688/Module/SystemData/YuJiangBoard.asmx";
 
 app.use( express.static( path.join( __dirname, 'web' ) ) );
 server.listen( 8000, function () {
@@ -39,10 +39,10 @@ app.get( '/room', function ( req, res ) {
 // } );
 
 io.on( 'connection', function ( socket ) {
-    var roomid = getParam( socket.request.headers.referer.split( '?' )[1], "v" )
+    var roomid = getParam( socket.request.headers.referer.split( '?' )[1], "v" );
     if ( typeof rooms[roomid] != 'undefined' ) {
         socket.join( roomid );    // 加入房间
-    }
+    };
     //socket.emit('news', { port: port });
     // socket.on('my other event', function(data) {
     //     console.log(data);
@@ -60,28 +60,28 @@ app.post( '/getMacStatus', function ( req, res ) {
 
     if ( month < 10 ) {
         month = '0' + month;
-    }
+    };
     if ( day < 10 ) {
         day = '0' + day;
-    }
+    };
     var datestring = year + month + day;
     var person = [
         '李峰', '张明', '王二小', '张晓', '李爱国', '李伟峰', '王杰', '迟中铭', '陆云', '刘三国', '刘永', '李国涛'
-    ]
+    ];
     var result = {
         status: 0,
         data: []
-    }
+    };
 
     for ( var i = 0; i < 23; i++ ) {
         var name = 'A';
         var key = i + 1;
         if ( key < 10 ) {
-            name = name + '00' + key
+            name = name + '00' + key;
         }
         else {
-            name = name + '0' + key
-        }
+            name = name + '0' + key;
+        };
         result.data.push( {
             mac_id: i + 1,
             mac_no: name,
@@ -92,8 +92,8 @@ app.post( '/getMacStatus', function ( req, res ) {
             yield: Math.round( Math.random() * 900 + 100 )
         } )
     }
-    res.send( result )
-} )
+    res.send( result );
+} );
 
 app.post( '/getAlarmInfo', function ( req, res ) {
     var result = {
@@ -102,27 +102,27 @@ app.post( '/getAlarmInfo', function ( req, res ) {
             total: 0,
             list: []
         }
-    }
+    };
     var date = new Date();
     var h = date.getHours();
     var m = date.getMinutes();
     var s = date.getSeconds();
-    result.data.total = parseInt(( h * 3600 + 60 * m + s ) / 80 )
+    result.data.total = parseInt(( h * 3600 + 60 * m + s ) / 80 );
 
     for ( var i = 0; i < parseInt( req.query.num ); i++ ) {
-        var ra1 = Math.round( Math.random() * 22 + 1 )
-        var ra2 = Math.round( Math.random() * 99 + 1 )
-        var name = 'A0' + numformate( ra1 )
-        var alarm = 'AR' + numformate( ra2 )
-        var time = date.getTime() - i * Math.round( Math.random() * 5 + 1 ) * 60 * 1000
+        var ra1 = Math.round( Math.random() * 22 + 1 );
+        var ra2 = Math.round( Math.random() * 99 + 1 );
+        var name = 'A0' + numformate( ra1 );
+        var alarm = 'AR' + numformate( ra2 );
+        var time = date.getTime() - i * Math.round( Math.random() * 5 + 1 ) * 60 * 1000;
         result.data.list.push( {
             name: name,
             alarm: alarm,
             time: time
         } )
     }
-    res.send( result )
-} )
+    res.send( result );
+} );
 
 app.post( '/getStatusRatio', function ( req, res ) {
     var result = {
@@ -133,18 +133,18 @@ app.post( '/getStatusRatio', function ( req, res ) {
             runTop: [],
             alarmTop: []
         }
-    }
+    };
     for ( var i = 0; i < parseInt( req.query.num ); i++ ) {
-        var name1 = 'A0' + numformate( i + 1 )
-        var value1 = parseFloat(( Math.round( Math.random() * 30 + 40 ) / 100 ).toFixed( 3 ) )
+        var name1 = 'A0' + numformate( i + 1 );
+        var value1 = parseFloat(( Math.round( Math.random() * 30 + 40 ) / 100 ).toFixed( 3 ) );
 
-        var name2 = 'A0' + numformate( i + 1 )
-        var value2 = parseFloat(( Math.round( Math.random() * 20 + 1 ) / 100 ).toFixed( 3 ) )
+        var name2 = 'A0' + numformate( i + 1 );
+        var value2 = parseFloat(( Math.round( Math.random() * 20 + 1 ) / 100 ).toFixed( 3 ) );
 
         result.data.runTop.push( {
             name: name1,
             value: value1
-        } )
+        } );
 
         result.data.alarmTop.push( {
             name: name2,
@@ -152,8 +152,8 @@ app.post( '/getStatusRatio', function ( req, res ) {
         } )
     }
 
-    res.send( result )
-} )
+    res.send( result );
+} );
 
 function numformate( n ) {
     if ( n < 10 ) {
@@ -162,15 +162,15 @@ function numformate( n ) {
     else {
         return n;
     }
-}
+};
 function getParam( url, name ) {
     var reg = new RegExp( "(^|&)" + name + "=([^&]*)(&|$)", "i" );
     var r = url.match( reg );
     if ( r != null ) return unescape( r[2] );
     return null;
-}
+};
 
-var room = ["a", "b"]
+var room = ["a", "b"];
 setInterval( function () {
     // getMacStatus_s( 'A', function ( res ) {
     //     console.log( res );
@@ -179,15 +179,15 @@ setInterval( function () {
     // getMacStatus_s( 'B', function ( res ) {
     //     io.to( 2 ).emit( 'getMacStatus', res )
     // } )
-}, 5000 )
+}, 5000 );
 
 setInterval( function () {
     getMacStatus( 'A', function ( res ) {
-        io.to( 1 ).emit( 'getMacStatus', res )
-    } )
+        io.to( 1 ).emit( 'getMacStatus', res );
+    } );
     getAlarmInfo( 'A', function ( res ) {
-        io.to( 1 ).emit( 'getAlarmInfo', res )
-    } )
+        io.to( 1 ).emit( 'getAlarmInfo', res );
+    } );
     // getAlarmInfo( 'B', function ( res ) {
     //     io.to( 2 ).emit( 'getAlarmInfo', res )
     // } )
@@ -196,46 +196,46 @@ setInterval( function () {
     // } )
     
     getYieldOfHour( 'A', function ( res ) {
-        io.to( 1 ).emit( 'getYieldOfHour', res )
-    } )
+        io.to( 1 ).emit( 'getYieldOfHour', res );
+    } );
     
     getRealData( 'A', function ( res ) {
-        io.to( 1 ).emit( 'getRealData', res )
-    })
+        io.to( 1 ).emit( 'getRealData', res );
+    } );
     // getStatusRatio( 'B', function ( res ) {
     //     io.to( 2 ).emit( 'getStatusRatio', res )
     // } )
-}, 50000 )
+}, 50000 );
 
 function getMacStatus_s( room, callback ) {
     request.get( server_url + '/getMacStatus',
         { key: 'value' }, function ( error, response, data ) {
             var d = JSON.parse( data );
-            console.log('/getMacStatus')
-            console.log(d.result)
-            callback( d.result )
+            console.log( '/getMacStatus' );
+            console.log( d.result );
+            callback( d.result );
         } )
-}
+};
 
 function getAlarmInfo_s( room, callback ) {
     request.get( server_url + '/getAlarmInfo',
         { key: 'value' }, function ( error, response, data ) {
             var d = JSON.parse( data );
-            console.log('/getAlarmInfo')
-            console.log(d.result)
-            callback( d.result )
+            console.log( '/getAlarmInfo' );
+            console.log( d.result );
+            callback( d.result );
         } )
-}
+};
 
 function getStatusRatio_s( room, callback ) {
     request.get( server_url + '/getStatusRatio',
         { key: 'value' }, function ( error, response, data ) {
             var d = JSON.parse( data );
-            console.log('/getStatusRatio')
-            console.log(d.result)
-            callback( d.result )
+            console.log( '/getStatusRatio' );
+            console.log( d.result );
+            callback( d.result );
         } )
-}
+};
 
 
 
@@ -256,11 +256,11 @@ function getMacStatus( room, callback ) {
     var datestring = year + month + day;
     var person = [
         '李峰', '张明', '王二小', '张晓', '李爱国', '李伟峰', '王杰', '迟中铭', '陆云', '刘三国', '刘永', '李国涛'
-    ]
+    ];
     var result = {
         status: 0,
         data: []
-    }
+    };
     var dt = [
         { no: 'OP05', name: '上料' },
         { no: 'OP10', name: '粗车小端' },
@@ -279,7 +279,7 @@ function getMacStatus( room, callback ) {
         { no: 'ROBOT1', name: '机械手1' },
         { no: 'ROBOT2', name: '机械手2' },
         { no: 'ROBOT3', name: '机械手3' }
-    ]
+    ];
     for ( var i = 0; i < dt.length; i++ ) {
         result.data.push( {
             //mac_id: i + 1,
@@ -290,9 +290,9 @@ function getMacStatus( room, callback ) {
             rhythm: Math.round( Math.random() * 10 + 75 ),
             yield: Math.round( Math.random() * 10 + 50 )
         } )
-    }
-    callback( result )
-}
+    };
+    callback( result );
+};
 
 function getAlarmInfo( room, callback ) {
     var result = {
@@ -301,12 +301,12 @@ function getAlarmInfo( room, callback ) {
             total: 0,
             list: []
         }
-    }
+    };
     var date = new Date();
     var h = date.getHours();
     var m = date.getMinutes();
     var s = date.getSeconds();
-    result.data.total = parseInt(( h * 3600 + 60 * m + s ) / 80 )
+    result.data.total = parseInt(( h * 3600 + 60 * m + s ) / 80 );
     var dt = [
         { no: 'OP05', name: '上料' },
         { no: 'OP10', name: '粗车小端' },
@@ -325,41 +325,41 @@ function getAlarmInfo( room, callback ) {
         { no: 'ROBOT1', name: '机械手1' },
         { no: 'ROBOT2', name: '机械手2' },
         { no: 'ROBOT3', name: '机械手3' }
-    ]
+    ];
     for ( var i = 0; i < parseInt( 10 ); i++ ) {
-        var ra1 = Math.round( Math.random() * 22 + 1 )
-        var ra2 = Math.round( Math.random() * 99 + 1 )
-        var name = dt[i].no
-        var alarm = room + 'R' + numformate( ra2 )
-        var time = date.getTime() - i * Math.round( Math.random() * 5 + 1 ) * 60 * 1000
+        var ra1 = Math.round( Math.random() * 22 + 1 );
+        var ra2 = Math.round( Math.random() * 99 + 1 );
+        var name = dt[i].no;
+        var alarm = room + 'R' + numformate( ra2 );
+        var time = date.getTime() - i * Math.round( Math.random() * 5 + 1 ) * 60 * 1000;
         result.data.list.push( {
             name: name,
             alarm: alarm,
             time: time
         } )
-    }
-    callback( result )
-}
+    };
+    callback( result );
+};
 
 function getYieldOfHour( room, callback ) {
     var result = {
         status: 0,
         data: []
-    }
-    var h = new Date().getHours()
+    };
+    var h = new Date().getHours();
     for ( var i = 0; i < 24; i++ ){
         if ( i < 8 ) {
-            result.data.push(0)
+            result.data.push( 0 );
         }
         else if ( i >= 8 && i <= h ) {
-            result.data.push(Math.round( Math.random() * 10 + 50 ))
+            result.data.push( Math.round( Math.random() * 10 + 50 ) );
         }
         else if ( i > h && i < 24 ) {
-            result.data.push(null)
+            result.data.push( null );
         }
-    }
-    callback( result )
-}
+    };
+    callback( result );
+};
 
 function getStatusRatio( room, callback ) {
     var result = {
@@ -370,26 +370,26 @@ function getStatusRatio( room, callback ) {
             runTop: [],
             alarmTop: []
         }
-    }
+    };
     for ( var i = 0; i < parseInt( 8 ); i++ ) {
-        var name1 = room + '0' + numformate( i + 1 )
-        var value1 = parseFloat(( Math.round( Math.random() * 30 + 40 ) / 100 ).toFixed( 3 ) )
+        var name1 = room + '0' + numformate( i + 1 );
+        var value1 = parseFloat(( Math.round( Math.random() * 30 + 40 ) / 100 ).toFixed( 3 ) );
 
-        var name2 = room + '0' + numformate( i + 1 )
-        var value2 = parseFloat(( Math.round( Math.random() * 20 + 1 ) / 100 ).toFixed( 3 ) )
+        var name2 = room + '0' + numformate( i + 1 );
+        var value2 = parseFloat(( Math.round( Math.random() * 20 + 1 ) / 100 ).toFixed( 3 ) );
 
         result.data.runTop.push( {
             name: name1,
             value: value1
-        } )
+        } );
 
         result.data.alarmTop.push( {
             name: name2,
             value: value2
-        } )
-    }
-    callback( result )
-}
+        } );
+    };
+    callback( result );
+};
 
 function getRealData( room, callback ) {
     var result = {
@@ -401,9 +401,9 @@ function getRealData( room, callback ) {
             qualified: Math.round( Math.random() * 5 + 90 ),
             achievement: Math.round( Math.random() * 5 + 80 )
         }
-    }
-    callback( result )
-}
+    };
+    callback( result );
+};
 // app.get('/test', function(req, res) {
 //     res.sendFile(__dirname + '/web/test.html');
 //     //res.render('')
